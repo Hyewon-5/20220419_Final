89cb0edab7e8d086e2e079b14f353c5ea3668beb
Hyewon-5 <belle7891@naver.com>
2024-12-10 18:46:51 +0900

    PLAY버튼 만들고 노래 재생, STOP하기

--- sketch.js
+++ sketch.js
@@ -1,7 +1,36 @@
+let mm;
+//let mm2;
+
+let amp;
+let vol;
+let button;
+
+function preload() {
+  soundFormats("mp3");
+  mm = loadSound("strategy.mp3");
+//  mm2 = loadSound("TickTack.mp3");
+}
+
+
 function setup() {
   createCanvas(640, 640);
+  
+  amp = new p5.Amplitude();
+  vol = 0.5;
+  button = createButton("PLAY");
+  button.mousePressed(playMusic);
 }
 
 function draw() {
   background(220);
+}
+
+function playMusic() {
+  if (!mm.isPlaying()) {
+    mm.play();
+    button.html("STOP");
+  } else {
+    mm.stop();
+    button.html("PLAY");
+  }
 }
