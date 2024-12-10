c7a91c2a013404acc330793cda01c92f5f14b029
Hyewon-5 <belle7891@naver.com>
2024-12-10 21:15:51 +0900

    두번째 플레이어 추가

--- sketch.js
+++ sketch.js
@@ -15,16 +15,33 @@ let img;
 let img2;
 
 
+let mm2;
+
+let amp2;
+let vol2;
+let button2;
+
+let jumpV2;
+
+let slider2;
+let sliderPan2;
+let sliderRate2;
+
+// let img3;
+// let img4;
+
+
 function preload() {
   soundFormats("mp3");
   mm = loadSound("strategy.mp3");
-//  mm2 = loadSound("TickTack.mp3");
+  mm2 = loadSound("TickTack.mp3");
 }
 
 
 function setup() {
   img2 = loadImage("stage.PNG");
   img = loadImage("twice.png");
+  
   
   createCanvas(640, 640);
   
@@ -44,12 +61,31 @@ function setup() {
   
   slider = createSlider(0, 2, 0.5, 0.1);
   sliderRate = createSlider(0, 2, 1, 0.1);
+  
+  
+  amp2 = new p5.Amplitude(); //2번째 노래
+  vol2 = 0.5;
+  button2 = createButton("PLAY");
+  button2.mousePressed(playMusic);
+  
+  buttonPause2 = createButton("PAUSE");
+  buttonPause2.mousePressed(pauseMusic);
+  
+  buttonJump2 = createButton("<<");
+  buttonJump2.mousePressed(jumpSong2);  
+  buttonJump2 = createButton(">>");
+  buttonJump2.mousePressed(jumpSong);
+  jumpV2 = 0;
+  
+  slider2 = createSlider(0, 2, 0.5, 0.1);
+  sliderRate2 = createSlider(0, 2, 1, 0.1);
 
 }
 
 function draw() {
-  img = loadImage("stage.PNG");
-  background(220);
+  image(img2, 0, 0, 640, 640);
+  //img2 = loadImage("stage.PNG");
+ // background(220);
   
   mm.setVolume(vol);
   vol = slider.value();
@@ -59,6 +95,17 @@ function draw() {
 }
 
 function playMusic() {
+  if (!mm.isPlaying()) {
+    mm.play();
+    button.html("STOP");
+  } else {
+    mm.stop();
+    button.html("PLAY");
+  }
+}
+
+
+function playMusic2() {
   if (!mm.isPlaying()) {
     mm.play();
     button.html("STOP");
@@ -92,3 +139,5 @@ function jumpSong2() {
   }
   mm.jump(jumpV);
 }
+
+
